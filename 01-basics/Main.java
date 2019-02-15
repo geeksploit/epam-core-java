774e8aea1d15c072db53649f6008401cb5f61c96
Max Rem <max@geeksploit.me>
2019-02-15 14:12:07 +0300

    docs: Comment test cases

--- 01-basics/Main.java
+++ 01-basics/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        QuadraticEquation.solve(0, 1, 1);
-        QuadraticEquation.solve(1, 1, 1);
-        QuadraticEquation.solve(1, 2, 1);
-        QuadraticEquation.solve(1, 4, 1);
+        QuadraticEquation.solve(0, 1, 1); // not a quadratic equation
+        QuadraticEquation.solve(1, 1, 1); // imaginary roots
+        QuadraticEquation.solve(1, 2, 1); // a single root
+        QuadraticEquation.solve(1, 4, 1); // two real roots
     }
 
 }
