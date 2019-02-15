e5e1a629820c2bf65e5134890576cb66de2bef3e
Max Rem <max@geeksploit.me>
2019-02-15 13:14:31 +0300

    feat: Detect an edge case when equation is not quadratic

--- 01-basics/QuadraticEquation.java
+++ 01-basics/QuadraticEquation.java
@@ -1,6 +1,12 @@
 public class QuadraticEquation {
 
     public static void solve(float a, float b, float c) {
+
+       if (Float.compare(a, 0)) {
+           System.out.println("Not a quadratic equation.");
+           return;
+       }
+      
     }
 
 }
