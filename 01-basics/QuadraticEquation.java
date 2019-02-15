6959af701a792acbfcd78488a0776d968e57edcf
Max Rem <max@geeksploit.me>
2019-02-15 14:21:41 +0300

    style: Fix indentation

--- 01-basics/QuadraticEquation.java
+++ 01-basics/QuadraticEquation.java
@@ -2,14 +2,14 @@ public class QuadraticEquation {
 
     public static void solve(float a, float b, float c) {
 
-      System.out.println(String.format("\na = %f; b = %f; c = %f", a, b, c));
+       System.out.println(String.format("\na = %f; b = %f; c = %f", a, b, c));
 
        if (Float.compare(a, 0) == 0) {
            System.out.println("Not a quadratic equation.");
            return;
        }
       
-      String out;
+       String out;
 
        float discriminant = b * b - 4 * a * c;
 
@@ -29,9 +29,9 @@ public class QuadraticEquation {
              break;
            default:
              out = "Something went wrong. Switch user and press any key.";
-       }
+        }
 
-       System.out.println(out);
+        System.out.println(out);
 
     }
 
