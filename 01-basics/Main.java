e04ba6ea3a49847b269381f489bd1c332877de38
Max Rem <max@geeksploit.me>
2019-02-15 14:03:42 +0300

    test: Attempt to solve a quadratic equation with a single root

--- 01-basics/Main.java
+++ 01-basics/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         QuadraticEquation.solve(0, 1, 1);
         QuadraticEquation.solve(1, 1, 1);
+        QuadraticEquation.solve(1, 2, 1);
     }
 
 }
