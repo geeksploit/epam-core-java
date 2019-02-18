0c6ad5e75c73429a6dd2ac796e1fee6066f0b34f
Max Rem <max@geeksploit.me>
2019-02-18 12:18:02 +0300

    feat: Define City constructor

--- 02-oop/src/me/geeksploit/tightcoupling/City.java
+++ 02-oop/src/me/geeksploit/tightcoupling/City.java
@@ -1,9 +1,21 @@
 package me.geeksploit.tightcoupling;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class City {
+
+  private static final Set<City> CITIES = new HashSet<>();
 
   private String name;
   private int population;
   private boolean isCapital;
+  private Country country;
+
+  public City(String name, int population) {
+    this.name = name;
+    this.population = population;
+    CITIES.add(this);
+  }
 
 }
