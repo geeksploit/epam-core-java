7bd14c4b730bb466ec39af22592e6237c7c5c343
Max Rem <max@geeksploit.me>
2019-02-18 12:23:19 +0300

    feat: Define Order constructor

--- 02-oop/src/me/geeksploit/tightcoupling/Order.java
+++ 02-oop/src/me/geeksploit/tightcoupling/Order.java
@@ -1,7 +1,27 @@
 package me.geeksploit.tightcoupling;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Order {
 
+  private static final Set<Order> ORDERS = new HashSet<>();
+
+  private final Set<Country> COUNTRIES = new HashSet<>();
+  private final Set<City> CITIES = new HashSet<>();
+
+  private Customer customer;
   private int price;
+
+  public Order(Customer customer, int price, City... cities) {
+    this.customer = customer;
+    this.price = price;
+    for (City c : cities) {
+      COUNTRIES.add(c.getCountry());
+    }
+    CITIES.addAll(Arrays.asList(cities));
+    ORDERS.add(this);
+  }
 
 }
