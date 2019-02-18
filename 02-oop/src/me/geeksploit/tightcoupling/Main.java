091412668ae6f519eb574d0669c495310905b889
Max Rem <max@geeksploit.me>
2019-02-18 13:32:11 +0300

    test: Define a couple of customers

--- 02-oop/src/me/geeksploit/tightcoupling/Main.java
+++ 02-oop/src/me/geeksploit/tightcoupling/Main.java
@@ -11,5 +11,9 @@ public class Main {
     City losAngeles = new City("Los Angeles", 4_000, usa);
     City tokyo = new City("Tokyo", 9_273, japan);
     City osaka = new City("Osaka", 2_691, japan);
+
+    Customer johnDoe = new Customer("John", "Doe", "1234567890");
+    Customer yokoNarayana = new Customer("Yoko", "Narayana", "234567890");
+    Customer janeEyre = new Customer("Jane", "Eyre", "345678901");
   }
 }
