0d6b73bbbc3bf0ff516f0a08448ebcb4487c1707
Max Rem <max@geeksploit.me>
2019-02-18 12:56:52 +0300

    feat: Define Country constructor

--- 02-oop/src/me/geeksploit/tightcoupling/Country.java
+++ 02-oop/src/me/geeksploit/tightcoupling/Country.java
@@ -5,4 +5,8 @@ public class Country {
   private String name;
   private String language;
 
+  public Country(String name, String language) {
+    this.name = name;
+    this.language = language;
+  }
 }
