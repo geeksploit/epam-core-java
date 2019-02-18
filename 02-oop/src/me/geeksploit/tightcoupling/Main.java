b859f3bb90a878bee3d6fde1fb5a32d7ca10adf7
Max Rem <max@geeksploit.me>
2019-02-18 13:31:13 +0300

    test: Define some countries

--- 02-oop/src/me/geeksploit/tightcoupling/Main.java
+++ 02-oop/src/me/geeksploit/tightcoupling/Main.java
@@ -3,6 +3,8 @@ package me.geeksploit.tightcoupling;
 public class Main {
 
   public static void main(String[] args) {
-    // write your code here
+
+    Country usa = new Country("USA", "English");
+    Country japan = new Country("Japan", "Japanese");
   }
 }
