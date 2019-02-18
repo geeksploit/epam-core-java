ca3248ec621d9e4b98627f53f7af4ddf04ecf58b
Max Rem <max@geeksploit.me>
2019-02-18 13:31:39 +0300

    test: Define a few cities

--- 02-oop/src/me/geeksploit/tightcoupling/Main.java
+++ 02-oop/src/me/geeksploit/tightcoupling/Main.java
@@ -6,5 +6,10 @@ public class Main {
 
     Country usa = new Country("USA", "English");
     Country japan = new Country("Japan", "Japanese");
+
+    City newYork = new City("New York", 8_623, usa);
+    City losAngeles = new City("Los Angeles", 4_000, usa);
+    City tokyo = new City("Tokyo", 9_273, japan);
+    City osaka = new City("Osaka", 2_691, japan);
   }
 }
