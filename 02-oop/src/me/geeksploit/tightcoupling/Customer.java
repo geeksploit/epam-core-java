87c6ef0dd7a553d4bc2561ccd6cbe12abb8a0388
Max Rem <max@geeksploit.me>
2019-02-18 11:45:50 +0300

    feat: Define Customer constructor

--- 02-oop/src/me/geeksploit/tightcoupling/Customer.java
+++ 02-oop/src/me/geeksploit/tightcoupling/Customer.java
@@ -1,9 +1,21 @@
 package me.geeksploit.tightcoupling;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Customer {
+
+  private static final Set<Customer> CUSTOMERS = new HashSet<>();
 
   private String firstName;
   private String lastName;
   private String passport;
+
+  public Customer(String firstName, String lastName, String passport) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.passport = passport;
+    CUSTOMERS.add(this);
+  }
 
 }
