866ebd9a0b894e3af689e5b3eb1116d2d1dce0c5
Max Rem <max@geeksploit.me>
2019-02-22 12:48:11 +0300

    feat: Reserve storage for customers

--- enot/src/me/geeksploit/enot/storage/Storage.java
+++ enot/src/me/geeksploit/enot/storage/Storage.java
@@ -1,5 +1,9 @@
 package me.geeksploit.enot.storage;
 
+import me.geeksploit.enot.customer.Customer;
+
 public class Storage {
     private static final int CAPACITY = 3;
+
+    public static Customer[] customers = new Customer[CAPACITY];
 }
