40bd356b3c6c93714179de98d85455f7c3094f96
Max Rem <max@geeksploit.me>
2019-02-22 13:08:13 +0300

    feat: Add customers to repository

--- enot/src/me/geeksploit/enot/customer/repo/CustomerMemoryRepo.java
+++ enot/src/me/geeksploit/enot/customer/repo/CustomerMemoryRepo.java
@@ -1,5 +1,20 @@
 package me.geeksploit.enot.customer.repo;
 
+import me.geeksploit.enot.customer.Customer;
+
+import static me.geeksploit.enot.storage.Storage.customers;
+
 public class CustomerMemoryRepo {
     private int customerIndex = -1;
+
+    public void addCustomer(Customer newCustomer) {
+        if (customerIndex == customers.length - 1) {
+            Customer[] newCustomersArray = new Customer[customers.length * 2];
+            System.arraycopy(customers, 0, newCustomersArray, 0, customers.length);
+            customers = newCustomersArray;
+        }
+
+        customerIndex++;
+        customers[customerIndex] = newCustomer;
+    }
 }
