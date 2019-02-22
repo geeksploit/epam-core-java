8dfe7734b5f5a7fda9c5ace51d7c567f56fc51c5
Max Rem <max@geeksploit.me>
2019-02-22 12:39:09 +0300

    feat: Define customer getters and setters

--- enot/src/me/geeksploit/enot/customer/Customer.java
+++ enot/src/me/geeksploit/enot/customer/Customer.java
@@ -15,4 +15,36 @@ public class Customer {
         this.lastName = lastName;
         this.age = age;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
