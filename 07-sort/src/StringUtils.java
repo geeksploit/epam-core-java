ca2e22d3a6417e4101c9d0b03a2b7e24c2739547
Max Rem <max@geeksploit.me>
2019-03-03 22:47:14 +0300

    feat: Loop until sorted

--- 07-sort/src/StringUtils.java
+++ 07-sort/src/StringUtils.java
@@ -8,6 +8,10 @@ public final class StringUtils {
 
     private static String[] suboptimalBubbleSort(String[] source, Comparator comparator) {
         String[] result = source.clone();
+        boolean sorted;
+        do {
+            sorted = true;
+        } while (!sorted);
         return result;
     }
 }
